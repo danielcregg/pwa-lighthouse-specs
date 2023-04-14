46db1c52118a90df26b4ba919f24722ea8804157
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-04-14 11:15:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
             }
         </script>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="theme-color" content="#317EFB"/>
     </head>
     <body>
         <h1>Hello world</h1>
