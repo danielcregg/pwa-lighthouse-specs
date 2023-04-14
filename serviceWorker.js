16971298de0145a1176d98e33823aaa8b9c5d5d4
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-04-14 10:50:59 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,55 @@
-var cacheName = 'pwa-app-cache';
-var filesToCache = ['/index.php'];
+const CACHE_NAME = 'smakowity-v1';
+const URLS_TO_CACHE = [
+    '/offline.html',
+    '/images/logo.png'
+]
 
-self.addEventListener("activate", (e) => {
-    console.log("[Service Worker] Activate")
-})
-
-self.addEventListener('install', function (e) {
-    console.log('[ServiceWorker] Install');
-    e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            console.log('[ServiceWorker] Caching app shell');
-            return cache.addAll(filesToCache);
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            return cache.addAll(URLS_TO_CACHE);
         })
     );
 });
 
-// this code shows add to home screen button in chrome
-self.addEventListener('fetch', (e) => {
-    console.log(e.request.url);
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request)),
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        // Get all available caches
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                // Delete old caches
+                if (CACHE_NAME.indexOf(key) === -1) {
+                    return caches.delete(key);
+                }
+            }));
+        })
     );
+});
+
+self.addEventListener('fetch', event => {
+    // Handle requests only from the app domain and pass the rest
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then(cache => {
+                // If the requested resource is in the cache
+                if (cache) {
+                    return cache;
+                }
+
+                return caches.open(CACHE_NAME).then(cache => {
+                    // Return the offline.html page in the offline mode
+                    if ( ! navigator.onLine) {
+                        return caches.match('/offline.html');
+                    }
+
+                    return fetch(event.request).then(response => {
+                        return response;
+                    }).catch(error => {
+                        console.warn(error);
+                    })
+                })
+            })
+        );
+    }
 });
