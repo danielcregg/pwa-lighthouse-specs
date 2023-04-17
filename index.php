4040f16d9569f26635ea2b9d99c684fcf0cb4eea
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-04-17 21:31:30 +0100

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>PWA App</title>
+		<link rel="manifest" href="./manifest.webmanifest">
+        <script>
+            if ("serviceWorker" in navigator) {
+                navigator.serviceWorker.register('./serviceWorker.js');
+            }
+        </script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="theme-color" content="#FFF"/>
+    </head>
+    <body>
+        <h1>Hello world</h1>
+        <?php
+            echo "<p>This is a PHP script.</p>";
+        ?>
+    </body>
+</html>
